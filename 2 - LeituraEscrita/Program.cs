09026e60f88c9d05fdc9bebae9e00761faccd7f7
Reniver Martins <r.kennyster@icloud.com>
2023-01-23 22:53:20 -0300

    Iniciando Estudos de Csharp

---
+++ 2 - LeituraEscrita/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*<<<Leitura>>>*/
+System.Console.WriteLine("Olá Reniver!");
+
+//console.writeLine
+System.Console.WriteLine("Texto com quebra de linha");
+
+//console.write
+System.Console.Write("Texto sem quebra de linha");
+System.Console.WriteLine("");
+
+
+
+/*<<<Escrita>>>*/
+var nome = Console.ReadLine();
+System.Console.WriteLine("Ola!"+nome);
---
+++ 3 - Variaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Variavel inicialada
+int idadeProfessor = 10;
+decimal salarioProfessor = 1000;
+string nomeProfessor = "Reniver Kennyster";
+
+
+//Variaveis não precisam ser inicializadas obrigatoriamente. 
+int idade ;
+decimal salario;
+string nome;
+
+    //Atribuindo valor após declaracao
+    nome = "Joel";
+
+    //atribuindo valor via input.
+    salario = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(salario);
+
+    //Declarando variavel no meio do fluxo
+    String email = "r.kennyster@icloud.com";
+    System.Console.WriteLine(email);
+
